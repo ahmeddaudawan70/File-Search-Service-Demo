38f33e704d3d650006c9c7568119bd6eb694366a
ahmeddaudawan70 <ahmeddaudawan70@gmail.com>
2025-07-15 14:19:55 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -2,4 +2,4 @@ GOOGLE_DRIVE_SCOPES = ['https://www.googleapis.com/auth/drive']
 CREDENTIALS_FILE = 'credentials.json'
 ELASTICSEARCH_HOST = 'https://localhost:9200'
 ELASTICSEARCH_USERNAME = 'elastic'
-ELASTICSEARCH_PASSWORD = 'ib_s=h4hmOiT_y-ux*P3'
+ELASTICSEARCH_PASSWORD = 'Your Elastic Search Password From the Terminal You Configured Your Elastic Search With'
